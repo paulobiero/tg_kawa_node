446a49653edd7d8696a985780196197d4d349dd7
paul obiero <paulobiero147@gmail.com>
2024-05-20 17:32:16 +0300

    Change serial

--- rtu_reader.js
+++ rtu_reader.js
@@ -52,7 +52,7 @@ const connectClient = function()
 const readModbusData = function()
 {
     // try to read data
-    client.readHoldingRegisters (305, 1)
+    client.readHoldingRegisters (3405, 1)
         .then(function(data)
         {
             mbsState   = MBS_STATE_GOOD_READ;
