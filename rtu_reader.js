f7b639d7e81d438d9cdacf76df6e0af2373eaba9
paul obiero <paulobiero147@gmail.com>
2024-05-20 17:31:07 +0300

    Change serial

--- rtu_reader.js
+++ rtu_reader.js
@@ -32,7 +32,7 @@ const connectClient = function()
     client.setTimeout (mbsTimeout);
 
     // try to connect
-    client.connectRTUBuffered ("/dev/ttyAMA0", { baudRate: 9600, parity: "even", dataBits: 8, stopBits: 1 })
+    client.connectRTUBuffered ("/dev/ttyS0", { baudRate: 9600, parity: "even", dataBits: 8, stopBits: 1 })
         .then(function()
         {
             mbsState  = MBS_STATE_GOOD_CONNECT;
